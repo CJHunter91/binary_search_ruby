2fb9d288ee2006464508735b0e49722fbd572023
Chris Hunter <orkem2@gmail.com>
2017-07-05 10:02:48 +0100

    Added linear and binary searc() and passes tests

--- binary_search_ruby.rb
+++ binary_search_ruby.rb
@@ -6,13 +6,28 @@ class BinarySearch
 	end
 
 
-  def binary_search()
-    high = (list.size) - 1
+  def binary_search(item)
+    high = (@list.size) - 1
     low = 0
 
-    while (low >= high)
-      return 
+    while (low <= high)
+      mid = (high + low) / 2
+      puts @list[mid]
+      if @list[mid] == item
+        return list[mid]
+      elsif @list[mid] < item
+        low = mid + 1
+      else
+        high = mid - 1 
+      end
+    end
+    return nil
+        
 
+  end
+
+  def linear_search(item)
+    @list.each{|number| return number if item == number}
   end
 
 	
--- specs/binary_search_ruby_spec.rb
+++ specs/binary_search_ruby_spec.rb
@@ -5,15 +5,21 @@ class TestBinarySearch < MiniTest::Test
 
 
 	def setup()
-		@search = BinarySearch.new([0..10])
+		@search = BinarySearch.new((0..10).to_a)
+    @search2 = BinarySearch.new((0..10000).to_a)
+    @search3 = BinarySearch.new((0..100000).to_a)
 	end
 
 	def test_init()
-		assert_equal([0..10],@search.list())
+		assert_equal((0..10).to_a,@search.list())
 	end
 
   def test_binary_search()
     assert_equal(3, @search.binary_search(3))
+  end
+
+  def test_linear_search()
+    assert_equal(9, @search.linear_search(9))
   end
 	
 end
