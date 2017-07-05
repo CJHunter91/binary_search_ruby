9da53801692d338fc36ed391c36d64acb23204ee
Chris Hunter <orkem2@gmail.com>
2017-07-05 08:11:50 +0100

    Added initial test to check the BinarySearch class is instansiated correctly

--- specs/binary_search_ruby_spec.rb
+++ specs/binary_search_ruby_spec.rb
@@ -1,10 +1,16 @@
 require('minitest/autorun')
-require_relative('../binary_search_spec.rb')
+require_relative('../binary_search_ruby.rb')
 
 class TestBinarySearch < MiniTest::Test
 
 
 	def setup()
-		
+		@search = BinarySearch.new([0..10])
 	end
+
+	def test_init()
+		assert_equal([0..10],@search.list())
+	end
+
+	
 end
