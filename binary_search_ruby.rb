487b30e29c5a729a0d5af41154965839b32f9aad
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-10 14:15:32 +0100

    Quart Search

--- binary_search_ruby.rb
+++ binary_search_ruby.rb
@@ -30,5 +30,26 @@ class BinarySearch
     @list.each{|number| return number if item == number}
   end
 
+  def quarte_search(item)
+    high = (@list.size) - 1.0
+    low = 0.0
+    item.to_f
+
+    # a + (b - a)/4 or b - (a - b)/4 
+    while (low <= high)
+      quart = low + (high - low) / 4.0
+      puts @list[quart]
+      if @list[quart] == item
+        return list[quart]
+      elsif @list[quart] < item
+        low = quart + 1.0
+      else
+        high = quart - 1.0
+      end
+    end
+    return nil
+  end
+
+
 	
 end
